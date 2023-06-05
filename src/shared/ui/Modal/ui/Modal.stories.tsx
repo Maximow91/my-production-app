ccb2f4e7a2ae9d0a21d9a797432a2e57b8a62699
kiryl_maximow <markizzzzka91@gmail.com>
2023-06-05 15:52:25 +0300

    added story for modal

--- src/shared/ui/Modal/ui/Modal.stories.tsx
+++ src/shared/ui/Modal/ui/Modal.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { Theme } from 'app/providers/ThemeProvider'
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorators'
 import { Modal } from './Modal'
 
 const meta: Meta<typeof Modal> = {
@@ -12,3 +14,9 @@ export default meta
 export const Primary: Story = {
     render: () => <Modal isOpen={true} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi tenetur eveniet odit porro! Velit accusamus neque, numquam magni animi fugit maiores ullam quis dolorum quisquam quas deleniti nobis. Iste?</ Modal>
 }
+
+export const Dark: Story = {
+    render: () => <Modal isOpen={true} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus eligendi tenetur eveniet odit porro! Velit accusamus neque, numquam magni animi fugit maiores ullam quis dolorum quisquam quas deleniti nobis. Iste?</ Modal>
+}
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
